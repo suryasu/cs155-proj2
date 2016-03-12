dfa2a787881450b0e096c854277a1cd5432025d5
Mimi Jiao <mimij2795@gmail.com>
2016-03-11 23:56:54 -0800

    Make words into a list, generates ~10 syllables lines now

--- nltk_baum_welch.py
+++ nltk_baum_welch.py
@@ -2,10 +2,21 @@ import string
 import os
 import numpy as np
 import nltk.tag.hmm as hmm
+from nltk.corpus import cmudict
+
+def num_syllables(word):
+    word = word.lower()
+    while word not in d:
+        word = word[:len(word)-1]
+    if len(word) == 0:
+        return 1
+    return max([len([y for y in x if y[-1].isdigit()]) for x in d[word]])
 
 directory = os.path.dirname(os.path.abspath(__file__))
 f = open(directory + "/shakespeare.txt", 'r')
-d = {}
+d = cmudict.dict()
+
+words = []
 index = 0
 order = []
 O = []
@@ -20,21 +31,23 @@ for line in f:
     line = line.split()
 
     for word in line:
-        if word not in d:
-            d[word] = index
-            index += 1
-        order.append((d[word], None))
+        try:
+            idx = words.index(word)
+        except ValueError:
+            idx = len(words)
+            words.append(word)
+        order.append((idx, None))
     O.append(np.array(order))
 
 num_samples = 20
 O = np.array(O)
-hmmt = hmm.HiddenMarkovModelTrainer(states=range(num_samples), symbols=range(len(d)))
+hmmt = hmm.HiddenMarkovModelTrainer(states=range(num_samples), symbols=range(len(words)))
 model = hmmt.train_unsupervised(O, max_iterations=50)
 
 # get probability of emission j given state i
 for i in range(num_samples):
     total = 0
-    for j in range(len(d)):
+    for j in range(len(words)):
         total += model._outputs[i].prob(j)
     assert (abs(total - 1.0) < 1e-6)
 
@@ -55,10 +68,16 @@ for i in range(num_samples):
 poem = ""
 for i in range(14):
     next_item = model._priors.generate()
-    poem += d.keys()[d.values().index(next_item)]
-    for j in range(9):
+    word = words[model._outputs[next_item].generate()]
+    poem += word
+    syllables = num_syllables(word)
+    while syllables < 10:
         next_item = model._transitions[next_item].generate()
-        poem += " " + d.keys()[d.values().index(model._outputs[next_item].generate())]
+        word = words[model._outputs[next_item].generate()]
+        while (syllables + num_syllables(word) > 10):
+            word = words[model._outputs[next_item].generate()]
+        poem += " " + word
+        syllables += num_syllables(word)
     poem += "\n"
 
 print poem
